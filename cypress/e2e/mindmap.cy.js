98d1d993ff4970bd98817d6d83a470b71a019305
AndreeaParpala <andreea.parpala@student.upt.ro>
2026-05-06 23:02:12 +0300

    update

--- cypress/e2e/mindmap.cy.js
+++ cypress/e2e/mindmap.cy.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 describe('Mindomo Cypress Tests', () => {
   const closeWhatsNewPopup = () => {
     cy.get('body').then(($body) => {
@@ -23,9 +23,7 @@ describe('Mindomo Cypress Tests', () => {
     cy.get('[placeholder="Username / E-mail"]').type(Cypress.env('email'))
     cy.get('[placeholder="Password"]').type(Cypress.env('password'), { log: false })
     cy.contains('button', 'Log in').click()
-    cy.wait(1000)
     cy.url().should('include', '/dashboard')
-    cy.wait(1000)
     closeWhatsNewPopup()
   }
 
@@ -45,9 +43,8 @@ describe('Mindomo Cypress Tests', () => {
     cy.get('[placeholder="Username / E-mail"]').type('invalid@test.com')
     cy.get('[placeholder="Password"]').type('WrongPassword123!', { log: false })
     cy.contains('button', 'Log in').click()
-cy.wait(1000)
+
     cy.url().should('include', '/login')
-    cy.wait(1000)
     cy.get('body').should('contain.text', 'Invalid login, please try again.')
   })
 
@@ -55,9 +52,8 @@ cy.wait(1000)
     cy.visit('https://www.mindomo.com/login')
 
     cy.contains('button', 'Log in').click()
-cy.wait(1000)
+
     cy.url().should('include', '/login')
-    cy.wait(1000)
     cy.get('body').should('contain.text', 'Invalid login, please try again.')
   })
 
@@ -65,104 +61,18 @@ cy.wait(1000)
     cy.visit('https://www.mindomo.com/login')
 
     cy.contains('Forgot password?').click()
-cy.wait(1000)
+
     cy.url().should('include', 'forgot')
     cy.get('body').should('contain.text', 'Forgot')
   })
 
-it('should open the AI Mind Maps section', () => {
+  it('should open the AI Mind Maps section', () => {
   login()
 
   cy.visit('https://www.mindomo.com/dashboard/create/ai/brainstorm')
-  cy.wait(1500)
   closeWhatsNewPopup()
 
-  
+  cy.get('body').should('contain.text', 'AI Brainstorm')
+  cy.get('body').should('contain.text', 'AI Mind Maps')
 })
-=======
-describe('Mindomo Cypress Tests', () => {
-
- it('should login and open the create templates page', () => {
-  cy.visit('https://www.mindomo.com/login')
-  cy.wait(1500)
-
-  cy.get('[placeholder="Username / E-mail"]').type(Cypress.env('email'))
-  cy.wait(1000)
-  cy.get('[placeholder="Password"]').type(Cypress.env('password'), { log: false })
-  cy.wait(1000)
-  cy.contains('button', 'Log in').click()
-  cy.wait(2000)
-  cy.url().should('include', '/dashboard')
-
-  cy.contains('Create').click({ force: true })
-  cy.wait(2000)
-
-  cy.contains('Blank mind map').should('be.visible')
-  cy.contains('Blank concept map').should('be.visible')
-})
-
-
-  it('should show an error message for invalid credentials', () => {
-    cy.visit('https://www.mindomo.com/login')
-    cy.wait(1500)
-
-    cy.get('[placeholder="Username / E-mail"]').type('invalid@test.com')
-    cy.wait(1000)
-
-    cy.get('[placeholder="Password"]').type('WrongPassword123!', { log: false })
-    cy.wait(1000)
-
-    cy.contains('button', 'Log in').click()
-    cy.wait(2000)
-
-    cy.url().should('include', '/login')
-    cy.get('body').should('contain.text', 'Invalid login, please try again.')
-    cy.wait(2000)
-  })
-
-
-  it('should not allow login with empty fields', () => {
-    cy.visit('https://www.mindomo.com/login')
-    cy.wait(1500)
-
-    cy.contains('button', 'Log in').click()
-    cy.wait(2000)
-
-    cy.url().should('include', '/login')
-    cy.get('body').should('contain.text', 'Invalid login, please try again.')
-    cy.wait(2000)
-  })
-it('should open the forgot password page', () => {
-  cy.visit('https://www.mindomo.com/login')
-  cy.wait(1500)
-  cy.contains('Forgot password?').click()
-  cy.wait(2000)
-  cy.url().should('include', 'forgot')
-  cy.get('body').should('contain.text', 'Forgot')
-})
-it('should allow user to input text in AI Mind Maps', () => {
-  cy.visit('https://www.mindomo.com/login')
-  cy.wait(1500)
-
-  cy.get('[placeholder="Username / E-mail"]').type(Cypress.env('email'))
-  cy.wait(1000)
-
-  cy.get('[placeholder="Password"]').type(Cypress.env('password'), { log: false })
-  cy.wait(1000)
-  cy.contains('button', 'Log in').click()
-  cy.wait(2000)
-  cy.url().should('include', '/dashboard')
-  cy.contains('Create').click({ force: true })
-  cy.wait(2000)
-  cy.contains('AI Mind Maps').click({ force: true })
-  cy.wait(2000)
-  cy.get('textarea').type('Project')
-
-  cy.get('textarea')
-    .should('be.visible')
-    .and('have.value', 'Project')
-
-  
-})
->>>>>>> bb2a92396a5974163a5a4d291614501a9a4a4d47
 })
